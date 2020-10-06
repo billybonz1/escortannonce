1bfeec4f8e1fb06a470d727a4141a58bcaca563d
Artem <kalini.art@gmail.com>
2020-10-06 21:53:44 +0300

    Страница профиля

---
+++ dist/login.9c9147a883c6e7aeaf53.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{Udtg:function(n,w){}},[["Udtg",0]]]);
+//# sourceMappingURL=login.9c9147a883c6e7aeaf53.js.map
--- src/profile.js
+++ src/profile.js
@@ -6,23 +6,23 @@ import Glide from '@glidejs/glide';
 
 new Glide('.glide', {
     perView: 5,
-  	focusAt: 'center',
     type: 'carousel',
     gap: 0,
     perTouch: 1,
+    animationDuration: 150,
     breakpoints: {
-        // 1126: {
-        //     perView: 4
-        // },
+        1200: {
+            perView: 3,
+        },
         // 960: {
         //     perView: 3
         // },
-        // 710: {
-        //     perView: 2
-        // },
-        // 460: {
-        //     perView: 1
-        // }
+        600: {
+            perView: 2
+        },
+        476: {
+            perView: 1
+        }
     }
 }).mount();
 
@@ -31,3 +31,29 @@ new Glide('.glide', {
 
 import { all } from './js/common.js';
 all(document);
+
+
+function scrollToTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollY will be -Infinity
+            scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+            if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = scrollY;
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
+
+document.querySelector(".up-btn").addEventListener("click", function (e) {
+	e.stopPropagation();
+   	scrollToTop(800);   
+});
